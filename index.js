c7b04ef43716f1a7bae240915e37556c53aae6ec
Ricardo Vega <ricardo@gumgum.com>
2018-07-04 21:16:19 -0500

    Initial commit

---
+++ index.js
@@ -0,0 +1,3 @@
+import DatePicker from "react-datepicker";
+
+export default DatePicker;
---
+++ rollup.config.js
@@ -0,0 +1,26 @@
+import commonjs from "rollup-plugin-commonjs";
+import resolve from 'rollup-plugin-node-resolve';
+import replace from "rollup-plugin-replace";
+
+export default {
+    input: 'index.js',
+    output: {
+        name: 'Datepicker',
+        file: 'dist/bundle.js',
+        format: 'iife'
+    },
+    external: ['react', 'react-dom', 'moment', 'classnames'],
+    globals: {
+        'react': 'React',
+        'react-dom': 'ReactDOM',
+        'classnames': 'classNames',
+        'moment': 'moment'
+    },
+    plugins: [
+        resolve(),
+        commonjs(),
+        replace({
+            'process.env.NODE_ENV': JSON.stringify( 'production' )
+        })
+    ]
+};
