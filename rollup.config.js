1966a71f1f9347ab417a4f2256f8857e87691e41
Vanessa Pacheco <nesspachecod@gmail.com>
2019-03-22 14:52:44 -0500

    Update dependecies and rollup file

--- rollup.config.js
+++ rollup.config.js
@@ -1,26 +1,26 @@
-import commonjs from "rollup-plugin-commonjs";
+import commonjs from 'rollup-plugin-commonjs';
 import resolve from 'rollup-plugin-node-resolve';
-import replace from "rollup-plugin-replace";
+import replace from 'rollup-plugin-replace';
 
 export default {
-    input: 'index.js',
-    output: {
-        name: 'Datepicker',
-        file: 'bundle.js',
-        format: 'iife'
-    },
-    external: ['react', 'react-dom', 'moment', 'classnames'],
+  input: 'index.js',
+  output: {
+    name: 'Datepicker',
+    file: 'bundle.js',
+    format: 'iife',
     globals: {
-        'react': 'React',
-        'react-dom': 'ReactDOM',
-        'classnames': 'classNames',
-        'moment': 'moment'
-    },
-    plugins: [
-        resolve(),
-        commonjs(),
-        replace({
-            'process.env.NODE_ENV': JSON.stringify( 'production' )
-        })
-    ]
+      react: 'React',
+      'react-dom': 'ReactDOM',
+      classnames: 'classNames',
+      moment: 'moment'
+    }
+  },
+  external: ['react', 'react-dom', 'moment', 'classnames'],
+  plugins: [
+    resolve(),
+    commonjs(),
+    replace({
+      'process.env.NODE_ENV': JSON.stringify('production')
+    })
+  ]
 };
